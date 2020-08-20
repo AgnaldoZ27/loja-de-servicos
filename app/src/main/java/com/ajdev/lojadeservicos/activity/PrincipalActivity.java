43e001914f924d94f4d59fadb5e75aae8d07819a
AgnaldoZ27 <agnaldojose.131@gmail.com>
2020-08-20 16:20:24 -0400

    Retorno

---
+++ app/src/main/java/com/ajdev/lojadeservicos/activity/PrincipalActivity.java
@@ -0,0 +1,16 @@
+package com.ajdev.lojadeservicos.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ajdev.lojadeservicos.R;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
---
+++ app/src/main/java/com/ajdev/lojadeservicos/model/Usuario.java
@@ -0,0 +1,61 @@
+package com.ajdev.lojadeservicos.model;
+
+import com.ajdev.lojadeservicos.config.ConfiguracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
+
+public class Usuario {
+
+    private String idUsuario;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+
+    }
+
+    public void salvar() {
+        DatabaseReference firebase = ConfiguracaoFirebase.getFirebaseDataBase();
+        firebase.child("users")
+                .child(this.idUsuario)
+                .setValue(this);
+    }
+
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
