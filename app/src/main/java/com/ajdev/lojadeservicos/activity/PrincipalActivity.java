4a2fc142c681fa205f8af516458321cec366847e
AgnaldoZ27 <agnaldojose.131@gmail.com>
2020-08-30 10:26:36 -0400

    Adicionado toolbar a tela principal e menu sair

--- app/src/main/java/com/ajdev/lojadeservicos/activity/PrincipalActivity.java
+++ app/src/main/java/com/ajdev/lojadeservicos/activity/PrincipalActivity.java
@@ -1,16 +1,59 @@
 package com.ajdev.lojadeservicos.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.ajdev.lojadeservicos.R;
+import com.ajdev.lojadeservicos.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        //configura toolbar
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("Loja de Serviços");
+        setSupportActionBar(toolbar);
+
+        //configurações de objetos
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_sair:
+                deslogarUsuario();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+        try {
+            autenticacao.signOut();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
